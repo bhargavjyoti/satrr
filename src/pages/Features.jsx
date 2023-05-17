64c6f00bcfe4c3213cfeddef9129306072f2cb21
Bhargav Kumar <116431763+bhargavjyoti@users.noreply.github.com>
2023-05-17 22:11:22 +0530

    Bug Fix

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -10,7 +10,7 @@ const Features = () => {
             <div className="border border-blue-700 shadow-lg p-4 md:w-1/3 text-center rounded-lg">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14 mx-auto text-blue-700 bg-white -mt-11">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-              <h2 className="text-xl text-center my-4">Patient Records Management</h2>
+              <h2 className="text-xl text-center my-4">Bills Management</h2>
               <p className="text-md text-center font-light md:w-3/4 mx-auto text-gray-500 leading-6">Simplify your billing process with our integrated billing and invoicing feature. Generate invoices, track payments, manage insurance claims etc.</p>
             </div>
             {/* Second */}
